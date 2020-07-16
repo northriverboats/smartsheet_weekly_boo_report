f0ff863d00edb015b01b1e4d97fbc70923d1d102
Fredrick W. Warren <fredw@northriverboats.com>
2020-07-16 10:26:51 -0700

    Handle .env files for pynstall setups

--- smartsheet_weekly_boats_on_order.py
+++ smartsheet_weekly_boats_on_order.py
@@ -7,12 +7,24 @@ import datetime
 import time
 import shutil
 import os
+import sys
 from dotenv import load_dotenv
 from click import echo
 from emailer import *
 
 
-load_dotenv()
+# load environmental variables
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+env_path = resource_path('.env')
+load_dotenv(dotenv_path=env_path)
 
 # GLOBALS start here
 all_dealers = os.getenv('DEALERS').split('|')
